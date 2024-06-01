02c36545bf2f7d00df0ddad728a8c58c1d94d586
jwrunge <jwrunge@gmail.com>
2024-05-31 23:17:45 -0500

    Update build

--- dist/mfld.js
+++ dist/mfld.js
@@ -3,9 +3,9 @@ function s(t){e.push(t),n||(n=!0,i(l))}function l(){n=!1
 for(let[t]of r)for(let[e,n]of t?.t||[])n?.(t.value,e)
 r.clear()
 for(let n of e)if("function"==typeof n)n()
-else{if([">","+"].includes(n.relation)){if(">"==n.relation){let t=document?.createElement("div")
+else{if(["$replace","$append"].includes(n.relation)){if("$replace"==n.relation){let t=document?.createElement("div")
 for(let e of Array.from(n.out?.childNodes||[]))t.appendChild(e)
-n.out?.replaceChildren(t),a(t,"out",n.ops)}t?.space?.(n.in,n.out),a(n.in,"in",n.ops,(()=>{n.in&&n.out?.appendChild(n.in),t?.adjust?.(n.in,n.ops)}))}else a(n.in,"in",n.ops,(()=>{n.out?.after(n.in),t?.space?.(n.in,n.out),t?.adjust?.(n.in,n.ops),"/"==n.relation&&a(n.out,"out",n.ops)}))
+n.out?.replaceChildren(t),a(t,"out",n.ops)}t?.space?.(n.in,n.out),a(n.in,"in",n.ops,(()=>{n.in&&n.out?.appendChild(n.in),t?.adjust?.(n.in,n.ops)}))}else a(n.in,"in",n.ops,(()=>{n.out?.after(n.in),t?.space?.(n.in,n.out),t?.adjust?.(n.in,n.ops),"$prepend"==n.relation&&a(n.out,"out",n.ops)}))
 n.done?.(n.in)}o.forEach((t=>t())),o=[],e=[]}function a(e,n,o,i){if(e?.nodeType==Node.TEXT_NODE){let t=e.textContent,n=document?.createElement("div")
 n.textContent=t,e.replaceWith(n),e=n}if(e){let r=Array.isArray(o.trans?.dur)?o.trans?.dur["in"==n?0:1]||o.trans?.dur[0]:o.trans?.dur||0,l=o?.trans?.class||"mf-trans"
 e?.classList?.add(l),o.trans?.hooks?.[`${n}-start`]?.(e),"out"==n?s((()=>{t?.size?.(e),r&&(e.style.transitionDuration=`${r}ms`),e.classList?.add(n)})):setTimeout((()=>{s((()=>{r&&(e.style.transitionDuration=`${r}ms`),e?.classList?.add(n),i?.(),s((()=>{e?.classList?.remove(n)}))}))}),o.trans?.swap||0),setTimeout((()=>{s((()=>{"out"==n&&e?.remove(),e?.classList?.remove(l),e?.classList?.remove(n),o.trans?.hooks?.[`${n}-end`]?.(e)}))}),r+("in"==n&&o.trans?.swap||0))}}function f(t){if(!t)return 0
@@ -26,14 +26,14 @@ if(o!==this.i){this.value=n,this.i=o,r.set(this,await t)
 for(let t of this.u)r.set(t,await t.p())
 s((()=>{e(this.value)}))}else e(this.value)}))}async p(){await this.update(await(this.o?.(Array.from(this.l)?.map((t=>t?.value))||[],this?.value))||this.value)}}function c(t,e){let n=MfSt.get(t)
 return e?n?n.h(t,e):new u(t,e):n||new u(t,e)}globalThis.DOMParser&&new DOMParser
-let h={},d="mf",p=/, {0,}/g,g=0
+let h={},d="mf",p=/, {0,}/g,$=0
 
 ;/**!
  * @param {Partial<MfldOps>} newops 
  * @param {string} [profileName] 
  */
-function $(t,e){e?h.profiles={...h.profiles,[e]:t}:h={...h,...t}}function y(t){let e=(t||document.body).querySelectorAll(`[data-${d}${["bind","sync","get","head","post","put","delete","patch"].join(`],[data-${d}`)}]${0!=h.fetch?.auto?",a":""}`)||[]
-for(let t of e){t.id||(t.id=""+g++)
+function g(t,e){e?h.profiles={...h.profiles,[e]:t}:h={...h,...t}}function y(t){let e=(t||document.body).querySelectorAll(`[data-${d}${["bind","sync","get","head","post","put","delete","patch"].join(`],[data-${d}`)}]${0!=h.fetch?.auto?",a":""}`)||[]
+for(let t of e){t.id||(t.id=""+$++)
 for(let e in t.dataset){let n=e!=`${d}bind`,o=`(#${t.id} on ${e})`
 t.dataset?.[e]?.split(";").forEach((i=>{let r,l=i?.split(/(?:(?:\)|->) ?){1,}/g)||[],a=n?w(l.splice(0,1)[0]):[],f=l[0]?.includes("(")&&l[0]?.match(/^[^\(]{1,}/)?.[0]||"",u=w(l.splice(e==`${d}sync`?1:0,1)[0]),h=w(l[0])
 if(n&&!a?.length)throw`No trigger: ${o}.`
@@ -54,7 +54,7 @@ void 0!==n&&o?.update?.((t=>p[0]?.path?.length?m(t,p[0]?.path,n):n))}
 for(let t of e)null==o&&(o="number"==typeof t?[]:{}),null==n||e[e.length-1]!==t?o=o instanceof Map?o?.get(t):o?.[t]:o instanceof Map?o.set(t,n):o[t]=n
 return o}function w(t){if(t?.includes("(")){let e=t.match(/[^\(\)]{1,}/g)
 t=e?.[e.length-1]||""}return t?.split(p)?.map((t=>t.trim()))||[]}function b(t,e,n,o,i){let r=async e=>{e?.preventDefault(),e?.stopPropagation()
-let r=t.dataset[`${d}overrides`]||"{}",l={...h.fetch,...h.profiles?.[r]?.fetch||JSON.parse(r)}
+let r=t.dataset[`${d}overrides`]||"{}",l={...h,...h.profiles?.[r]?.fetch||JSON.parse(r)}
 i||(i="string"==typeof o?structuredClone(o):(e?.target)?.href,o=l?.request?.body),n||(n=(e?.target)?.method||"get")
 let a=l
 l?.externals?.find((t=>i?.startsWith(t.domain)))||!i.match(/^https?:\/\//)||i.includes(location.origin)
@@ -169,7 +169,7 @@ funcs=>{for(let t in funcs)MfFn.set(t,funcs[t])},config:
  * @param {MfldOps} new_ops
  * @param {string} [profile_name]
  */
-(new_ops,profile_name)=>$(new_ops,profile_name),onTick:
+(new_ops,profile_name)=>g(new_ops,profile_name),onTick:
 /**!
   * - Wait for the next Manifold data update cycle to complete before executing the callback function.
   * @param {()=> void} cb
@@ -182,4 +182,4 @@ t=>{var e;(e=t)&&o.push(e)},register:
 t=>{"string"==typeof t&&(t=document.querySelector(t)),y(t)}}
 globalThis.Mfld=M
 let v=globalThis.document?.currentScript?.dataset||{}
-if(v?.config)try{$(JSON.parse(v?.config))}catch(t){console.warn("Invalid Mfld params",t)}v?.init&&y(document.querySelector(v.init))
+if(v?.config)try{g(JSON.parse(v?.config))}catch(t){console.warn("Invalid Mfld params",t)}v?.init&&y(document.querySelector(v.init))
