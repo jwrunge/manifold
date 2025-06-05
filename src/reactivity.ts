5bea616aa5233b63c5133214f939db3194c1ecf9
jwr <jwrunge@gmail.com>
2025-06-04 23:50:15 -0500

    Exploring store fixes

--- src/reactivity.ts
+++ src/reactivity.ts
@@ -1,72 +1,44 @@
-import { register } from "./registry";
-
-type UpdaterPayload = {
-	$st: typeof $st;
-	$fn: typeof $fn;
-	$el?: WeakRef<HTMLElement>;
-};
-
-type UpdaterFn<T> = (p: UpdaterPayload) => T;
-
-type StoreOptions<T> = {
-	value?: T;
-	updater?: string | UpdaterFn<T>;
-	upstream?: Store[];
-	scope?: WeakRef<HTMLElement>;
-};
-
-globalThis.mfld ??= {
-	st: new WeakMap<HTMLScriptElement, Map<string, Store>>(),
-	fn: new WeakMap<HTMLScriptElement, Map<string, () => void>>(),
-	tick: [],
-};
-
-export const $st = new Proxy(mfld.st, {
-	get: (stores, property: string) => stores.get(property)?.deref(),
-	set: (stores, property: string, value) => {
-		stores.get(property)?.deref()?.update(value);
-		return true;
-	},
-});
-
-export const $fn = new Proxy(mfld.fn, {
-	get: (funcs, property: string) => funcs.get(property)?.deref(),
-	set: (funcs, property: string, value: () => void) => {
-		funcs.set(property, new WeakRef(value));
-		return true;
-	},
-});
-
-export class Store<T = unknown> {
-	id: number | string;
+type StoreOptionsWithValue<T> = {
 	value: T;
-	readonly updater?: (p: UpdaterPayload) => T;
-	#el?: WeakRef<HTMLElement>;
+	updater: undefined;
+	upstream: undefined;
+};
+
+type StoreOptionsWithFunc<T> = {
+	value: undefined;
+	updater: (upstream: Store[]) => T;
+	upstream?: Store[];
+};
+
+type StoreOptions<T> = StoreOptionsWithValue<T> | StoreOptionsWithFunc<T>;
+
+type Updater<T> = (upstream: Store[]) => T;
+
+class Store<T = unknown> {
+	value: T;
+	#updater?: Updater<T>;
 	#upstream = new Set<WeakRef<Store>>();
 	#downstream = new Set<WeakRef<Store>>();
 	#hash: number | null = null;
-	#remap: [string, string] | [] = [];
 	static updateSet: Set<WeakRef<Store>> = new Set();
 	static cancelAnim = 0;
 
 	constructor(ops: StoreOptions<T>) {
-		this.id = Math.random();
-		this.updater = ops.updater;
-		for (const ups of ops.upstream ?? [])
-			this.#upstream.add(new WeakRef(ups));
-		for (const dep of ops.deps ?? []) this.#deps.add(new WeakRef(dep));
-		this.value = ops.value;
+		const { value, updater, upstream = [] } = ops;
+		this.#updater = updater;
+		for (const u of upstream) this.#upstream.add(new WeakRef(u));
+		this.value = this.update(value ?? updater);
 	}
 
 	#changed(input: unknown): boolean {
-		if (typeof input !== "object") return input === this.value; // Direct compare primatives
-		if ([Map, Set].some((c) => input instanceof c))
+		if (typeof input !== "object") return input === this.value;
+		if (input instanceof Map || input instanceof Set)
 			return this.#changed([
 				...(input as Map<unknown, unknown> | Set<unknown>).entries(),
 			]);
 
 		const h = Array.from(
-			new TextEncoder().encode(input?.toString() || "")
+			new TextEncoder().encode(String(input || ""))
 		).reduce((hash, char) => (hash << 5) - hash + char, 0);
 
 		const changed = h !== this.#hash;
@@ -74,55 +46,35 @@ export class Store<T = unknown> {
 		return changed;
 	}
 
-	#parseFn(
-		fn?: UpdaterFn<T> | string,
-	): [UpdaterFn<T> | undefined, [string, string] | [], Set<WeakRef<Store>>] {
-		if (typeof fn !== "string") return [fn, [], new Set()];
-
-		const [fnStr, asStr] = fn.split(rxSeparateFnFromIterator);
-		const func = new Function("ops", fnStr) as UpdaterFn<T>;
-		const dependencies = new Set<WeakRef<Store>>();
-		return [
-			func,
-			(asStr?.split(rxCommaSeparator) as [string, string]) ?? [],
-			dependencies,
-		];
-	}
-
-	_reEval(updateSet?: Set<WeakRef<Store>>) {
-		// Don't update if upstream store is in updateSet
-		for (const up of this.#upstream) if (updateSet?.has(up)) return;
-
-		// Update value and propagate changes
-		return this.update(
-			this.updater?.({
-				$st,
-				$fn,
-				$el: this.#el,
-			})
-		);
-	}
-
-	update(value?: T) {
-		if (value === undefined) return;
+	update(value?: T | Updater<T>): T {
 		const newValue =
-			typeof value === "function" ? value(this.value) : value;
+			value instanceof Function
+				? value(
+						[...this.#upstream]
+							.map((u) => u.deref())
+							.filter((u) => !!u)
+				  )
+				: value ?? this.value;
 
 		if (this.#changed(newValue)) {
 			this.value = newValue;
 			for (const down of this.#downstream) {
 				Store.updateSet.add(down);
-				Store.cancelAnim ||= requestAnimationFrame(() => runUpdates());
+				Store.cancelAnim ||= requestAnimationFrame(() =>
+					Store.#runUpdates()
+				);
 			}
 		}
 
 		return this.value;
 	}
 
-	static #scheduleUpdate = (update: WeakRef<Store>) => {
-		Store.updateSet.add(update);
-		Store.cancelAnim ||= requestAnimationFrame(() => Store.#runUpdates());
-	};
+	_reEval(updateSet?: Set<WeakRef<Store>>) {
+		if (this.#updater) {
+			for (const up of this.#upstream) if (updateSet?.has(up)) return;
+			return this.update(this.#updater ?? this.value);
+		}
+	}
 
 	static #runUpdates(recursed = 0) {
 		if (recursed + 1 > 100)
@@ -131,11 +83,11 @@ export class Store<T = unknown> {
 			);
 		Store.cancelAnim = 0;
 
-	const newUpdateSet: Set<WeakRef<Store>> = new Set();
-	for (const store of Store.updateSet) {
-		//If the store has an upstream store that is contained in updateSet, don't add to newUpdateSet
-		store.deref()?._reEval(newUpdateSet);
-	}
+		const newUpdateSet: Set<WeakRef<Store>> = new Set();
+		for (const store of Store.updateSet) {
+			//If the store has an upstream store that is contained in updateSet, don't add to newUpdateSet
+			store.deref()?._reEval(newUpdateSet);
+		}
 
 		if (newUpdateSet.size) Store.updateSet = newUpdateSet;
 		else {
@@ -143,5 +95,6 @@ export class Store<T = unknown> {
 			mfld.tick = [];
 		}
 
-	runUpdates(recursed);
-};
+		Store.#runUpdates(recursed);
+	}
+}
