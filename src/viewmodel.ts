c5d7f9e0105bed815c685491579221ee8d0dc1f2
jwr <jwrunge@gmail.com>
2025-06-17 00:04:21 -0500

    Trying to type listeners

--- src/viewmodel.ts
+++ src/viewmodel.ts
@@ -1,5 +1,56 @@
 import { State } from "./reactivity";
 
+type EventMap = {
+	// Mouse Events
+	onclick: MouseEvent;
+	ondblclick: MouseEvent;
+	onmousedown: MouseEvent;
+	onmouseup: MouseEvent;
+	onmouseover: MouseEvent;
+	onmousemove: MouseEvent;
+	onmouseout: MouseEvent;
+	onmouseenter: MouseEvent;
+	onmouseleave: MouseEvent;
+	// Keyboard Events
+	onkeydown: KeyboardEvent;
+	onkeyup: KeyboardEvent;
+	onkeypress: KeyboardEvent;
+	// Form Events
+	onchange: Event;
+	oninput: Event;
+	onsubmit: SubmitEvent;
+	onfocus: FocusEvent;
+	onblur: FocusEvent;
+	// Drag Events
+	ondrag: DragEvent;
+	ondragend: DragEvent;
+	ondragenter: DragEvent;
+	ondragleave: DragEvent;
+	ondragover: DragEvent;
+	ondragstart: DragEvent;
+	ondrop: DragEvent;
+	// Touch Events
+	ontouchstart: TouchEvent;
+	ontouchmove: TouchEvent;
+	ontouchend: TouchEvent;
+	ontouchcancel: TouchEvent;
+	// Other Common Events
+	onscroll: Event;
+	onload: Event;
+	onerror: ErrorEvent;
+};
+
+type EventListenerWithTarget<
+	TEvent extends Event,
+	TElement extends EventTarget
+> = (event: TEvent & { target: TElement }) => void;
+
+type WithTypedListeners<T extends HTMLElement | SVGElement | MathMLElement> = {
+	[K in keyof T]?: K extends keyof EventMap
+		? EventListenerWithTarget<EventMap[K], T>
+		: T[K];
+};
+
 export type DeepPartial<T> = {
 	[P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
 };
